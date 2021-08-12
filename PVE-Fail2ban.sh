bb8f752676b00aa8e16e0a8945b4d66dc7fc8b38
Octo System <50835071+OcotoSystem@users.noreply.github.com>
2021-08-12 17:50:14 +0200

    Update PVE-Fail2ban.sh

--- PVE-Fail2ban.sh
+++ PVE-Fail2ban.sh
@@ -1,29 +1,24 @@
-#!/bin/bash
 
-lo
+#!/bin/bash
 LOCAL_INSTALL=0
 DEFAULT_DOWNLOAD_DIR="/etc/";
+#install block
 
-download_Fail2Ban()
-{
 cd $DEFAULT_DOWNLOAD_DIR;
-if [ $LOCAL_INSTALL == 0 ]; then
-   if [ -r $1 ];
-      then echo "$1 is already downloaded";
-    else apt-get install fail2ban;
-    fi;
-  fi;
-}
+apt-get install fail2ban
+cat >/etc/fail2ban/jail.local <<EOL
+  [proxmox-web-gui]
+  enabled  = true
+  port     = http,https,8006
+  filter   = proxmox-web-gui
+  logpath  = /var/log/daemon.log
+  maxretry = 4
+  bantime = 240
+EOL
 
-
-modify_jail_local()
-  nano /etc/fail2ban/jail.local
-  #incrémentéeça
-[proxmox-web-gui]
-enabled  = true
-port     = http,https,8006
-filter   = proxmox-web-gui
-logpath  = /var/log/daemon.log
-maxretry = 4
-bantime = 240
-{
+#filter testing
+fail2ban-regex /var/log/daemon.log /etc/fail2ban/filter.d/proxmox-web-gui.conf
+echo "filter testing";
+#test du service
+service fail2ban restart
+fail2ban-client -v status
